0a4b3a0626d8f893a87041e7fc1c56d95cf0343c
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-16 10:32:03 -0600

    Fixed.

--- lib/testing/game_test.py
+++ lib/testing/game_test.py
@@ -96,6 +96,7 @@ class TestGame:
         player = Player("Nick")
         player_2 = Player("Ari")
         Result(player, game, 5000)
+        # Result(player, game, 5002) Conflicting with test that checks score is not above 5000 and I believe this was an oversight.  Adding a new test to correct.
         Result(player, game, 4998)
         Result(player_2, game, 4999)
 
