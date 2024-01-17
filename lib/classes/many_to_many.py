cbb06e894f82f37e9d5df167e4188b92db501cd3
Jeffrey Davis <j.davis081248@gmail.com>
2024-01-17 14:34:06 -0600

    Reset new branch to empty code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,120 +1,34 @@
 class Game:
     def __init__(self, title):
         self.title = title
-    
-    @property
-    def title(self):
-        return self._title
-    
-    @title.setter
-    def title(self, value):
-
-        if isinstance(value, str) and 0 < len(value) and not hasattr(self, "title"):
-            self._title = value
-        else:
-            raise Exception
 
     def results(self):
-        return [result for result in Result.all if result.game == self]
+        pass
 
     def players(self):
-        return list(set(result.player for result in self.results()))
+        pass
 
     def average_score(self, player):
-        
-        player_avgs = [result.score for result in self.results() if player == result.player]
-
-        return sum(player_avgs)/len(player_avgs)
+        pass
 
 class Player:
-
-    all = []
-
     def __init__(self, username):
         self.username = username
 
-        Player.all.append(self)
-    
-    @property
-    def username(self):
-        return self._username
-
-    @username.setter
-    def username(self, value):
-        if isinstance(value, str) and 1 < len(value) < 17:
-            self._username = value
-        else:
-            raise Exception
-
     def results(self):
-        return [result for result in Result.all if result.player == self]
+        pass
 
-    def games_played(self): #gets unique list of games played by player
-        return list(set(result.game for result in self.results()))
+    def games_played(self):
+        pass
 
     def played_game(self, game):
-        if game in self.games_played():
-            return True
-        else:
-            return False
+        pass
 
     def num_times_played(self, game):
-        return len([result for result in self.results() if result.game == game])
-    
-    @classmethod
-    def highest_scored(cls, game):
-        best_player = None
-        highest_score = 0
-
-        for player in cls.all:
-            if player in game.players():
-                if highest_score < game.average_score(player):
-                    highest_score = game.average_score(player)
-                    best_player = player
-
-        return best_player
+        pass
 
 class Result:
-
-    all = []
-
     def __init__(self, player, game, score):
         self.player = player
         self.game = game
         self.score = score
-
-        Result.all.append(self)
-
-    @property
-    def score(self):
-        return self._score
-
-    @score.setter
-    def score(self, value):
-        if isinstance(value, int) and 0 < value < 5001 and not hasattr(self, "score"):
-            self._score = value
-        else:
-            raise Exception
-    
-    @property
-    def player(self):
-        return self._player
-
-    @player.setter
-    def player(self, value):
-        if isinstance(value, Player):
-            self._player = value
-        else:
-            raise Exception
-    
-    @property
-    def game(self):
-        return self._game
-    
-    @game.setter
-    def game(self, value):
-        if isinstance(value, Game):
-            self._game = value
-        else:
-            raise Exception
-    
